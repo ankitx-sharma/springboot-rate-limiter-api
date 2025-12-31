0f474fc4f2a239cbc4abaa1d829607d931f5d9ec
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-31 12:16:41 +0100

    added bean configuration for rest template

--- src/main/java/com/project/rate_limiter/RateLimiterApplication.java
+++ src/main/java/com/project/rate_limiter/RateLimiterApplication.java
@@ -2,6 +2,9 @@ package com.project.rate_limiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RateLimiterApplication {
@@ -10,4 +13,8 @@ public class RateLimiterApplication {
 		SpringApplication.run(RateLimiterApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
