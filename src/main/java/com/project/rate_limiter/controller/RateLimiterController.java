2905c9a58bf0efe53cea4ca8135e40082a77102c
Ankita Sharma <ankitasharma.281993@gmail.com>
2026-01-01 13:32:16 +0100

    removed this controller api from swagger-ui

--- src/main/java/com/project/rate_limiter/controller/RateLimiterController.java
+++ src/main/java/com/project/rate_limiter/controller/RateLimiterController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/limiter/api")
 public class RateLimiterController {
 	
+	@Operation(hidden = true)
 	@GetMapping("/check")
 	public ResponseEntity<String> isAllowed(){
 		return ResponseEntity.ok("Request Allowed");
